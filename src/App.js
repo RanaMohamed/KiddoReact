da0c05ca6e4d6a2550582dd809e29434fe3f5000
AmalAtef <AmalAtef@users.noreply.github.com>
2020-06-18 18:07:43 +0200

    Filter

--- src/App.js
+++ src/App.js
@@ -439,6 +439,21 @@ function App() {
         </div>
       </div>
 			
+{/* Filter*/}
+<div  style={{ margin: '3rem'}} >
+<ul className="filter">
+    <li className="filter-item">All Categories</li>
+	<li className="filter-item filter-item--selected filter-item--before ">Gardening</li>
+	<li className="filter-item ">Arts & Crafts</li>
+	<li className="filter-item ">Cooking</li>
+	<li className="filter-item  filter-item--selected filter-item--before ">Writing</li>
+	<li className="filter-item  filter-item--selected filter-item--before ">Drawing</li>
+	<li className="filter-item ">Photography</li>
+	<li className="filter-item ">Singing</li>
+	<li className="filter-item ">Knitting</li>
+</ul>
+</div>
+
 		</React.Fragment>
 	);
 }
