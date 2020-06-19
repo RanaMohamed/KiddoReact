be0629e2d068a56ec168c97e2805b349ca65f8ac
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-19 22:31:20 +0200

    :hankey: Add Redux with dummy actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,17 @@ import './css/main.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<BrowserRouter>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</BrowserRouter>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
