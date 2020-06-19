a7bbf6f2fff6656b69dbefff13a4a76e427ec257
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-19 04:38:42 +0200

    :pencil2: App import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/main.css';
 import '../node_modules/@fortawesome/fontawesome-free/css/all.css';
-import App from './App.jsx';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
