fd4752820f26596bf3c44b3120dc7dc8d4fcaaa9
AmalAtef <AmalAtef@users.noreply.github.com>
2020-06-19 11:39:16 +0200

    Merge

--- src/App.js
+++ src/App.js
@@ -43,6 +43,248 @@ function App() {
 
       <div className="App">Kiddo</div>
 
+      <div
+        className="container"
+        style={{
+          display: "grid",
+          gridTemplateColumns: "auto auto auto",
+          gap: "10rem"
+        }}
+      >
+        <div className="post-card post-card--bg post-card--bg--primary">
+          <div className="post-card__img">
+            <img alt="Post" src="../img/post1.png" />
+          </div>
+          <div className="post-card__body">
+            <p className="post-card__title">Post 1</p>
+            <p>
+              Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+              interpretaris, eam an quem pericula. Mei te aliquid mediocritatem.
+              Sint rebum repudiandae vel ei. In mollis fuisset fierent pro,
+              habemus dolores tincidunt ad nam. Vis laudem nostrud ex, augue
+              errem indoctum ea eam.
+            </p>
+            <div className="post-card__info">
+              <img
+                className="post-card__avatar"
+                alt="Avatar"
+                src="../img/avatar.png"
+              />
+              <div>
+                <p>Full name</p>
+                <i className="fa fa-heart like-icon"></i>
+                <span>20</span>
+                <i className="fa fa-comment comment-icon"></i>
+                <span>10</span>
+              </div>
+            </div>
+            <div className="post-card__overlay">
+              <button>View Details</button>
+              <button>Button2</button>
+            </div>
+          </div>
+        </div>
+        <div className="post-card post-card--bg post-card--bg--1">
+          <div className="post-card__img">
+            <img alt="Post" src="../img/post1.png" />
+          </div>
+          <div className="post-card__body">
+            <p className="post-card__title">Post 1</p>
+            <p>
+              Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+              interpretaris, eam an quem pericula. Mei te aliquid mediocritatem.
+              Sint rebum repudiandae vel ei. In mollis fuisset fierent pro,
+              habemus dolores tincidunt ad nam. Vis laudem nostrud ex, augue
+              errem indoctum ea eam.
+            </p>
+            <div className="post-card__info">
+              <img
+                className="post-card__avatar"
+                alt="Avatar"
+                src="../img/avatar.png"
+              />
+              <div>
+                <p>Full name</p>
+                <i className="fa fa-heart like-icon"></i>
+                <span>20</span>
+                <i className="fa fa-comment comment-icon"></i>
+                <span>10</span>
+              </div>
+            </div>
+            <div className="post-card__overlay">
+              <button>View Details</button>
+              <button>Button2</button>
+            </div>
+          </div>
+        </div>
+        <div className="post-card post-card--bg post-card--bg--2">
+          <div className="post-card__img">
+            <img alt="Post" src="../img/post1.png" />
+          </div>
+          <div className="post-card__body">
+            <p className="post-card__title">Post 1</p>
+            <p>
+              Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+              interpretaris, eam an quem pericula. Mei te aliquid mediocritatem.
+              Sint rebum repudiandae vel ei. In mollis fuisset fierent pro,
+              habemus dolores tincidunt ad nam. Vis laudem nostrud ex, augue
+              errem indoctum ea eam.
+            </p>
+            <div className="post-card__info">
+              <img
+                className="post-card__avatar"
+                alt="Avatar"
+                src="../img/avatar.png"
+              />
+              <div>
+                <p>Full name</p>
+                <i className="fa fa-heart like-icon"></i>
+                <span>20</span>
+                <i className="fa fa-comment comment-icon"></i>
+                <span>10</span>
+              </div>
+            </div>
+            <div className="post-card__overlay">
+              <button>View Details</button>
+              <button>Button2</button>
+            </div>
+          </div>
+        </div>
+        <div className="post-card post-card--bg post-card--bg--3">
+          <div className="post-card__img">
+            <img alt="Post" src="../img/post1.png" />
+          </div>
+          <div className="post-card__body">
+            <p className="post-card__title">Post 1</p>
+            <p>
+              Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+              interpretaris, eam an quem pericula. Mei te aliquid mediocritatem.
+              Sint rebum repudiandae vel ei. In mollis fuisset fierent pro,
+              habemus dolores tincidunt ad nam. Vis laudem nostrud ex, augue
+              errem indoctum ea eam.
+            </p>
+            <div className="post-card__info">
+              <img
+                className="post-card__avatar"
+                alt="Avatar"
+                src="../img/avatar.png"
+              />
+              <div>
+                <p>Full name</p>
+                <i className="fa fa-heart like-icon"></i>
+                <span>20</span>
+                <i className="fa fa-comment comment-icon"></i>
+                <span>10</span>
+              </div>
+            </div>
+            <div className="post-card__overlay">
+              <button>View Details</button>
+              <button>Button2</button>
+            </div>
+          </div>
+        </div>
+        <div className="post-card post-card--bg post-card--bg--4">
+          <div className="post-card__img">
+            <img alt="Post" src="../img/post1.png" />
+          </div>
+          <div className="post-card__body">
+            <p className="post-card__title">Post 1</p>
+            <p>
+              Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+              interpretaris, eam an quem pericula. Mei te aliquid mediocritatem.
+              Sint rebum repudiandae vel ei. In mollis fuisset fierent pro,
+              habemus dolores tincidunt ad nam. Vis laudem nostrud ex, augue
+              errem indoctum ea eam.
+            </p>
+            <div className="post-card__info">
+              <img
+                className="post-card__avatar"
+                alt="Avatar"
+                src="../img/avatar.png"
+              />
+              <div>
+                <p>Full name</p>
+                <i className="fa fa-heart like-icon"></i>
+                <span>20</span>
+                <i className="fa fa-comment comment-icon"></i>
+                <span>10</span>
+              </div>
+            </div>
+            <div className="post-card__overlay">
+              <button>View Details</button>
+              <button>Button2</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container" style={{ margin: "2rem auto" }}>
+        <div className="post-card post-card--md post-card--bg post-card--bg--4">
+          <div className="post-card__post">
+            <div className="post-card__img">
+              <img alt="Post" src="../img/post1.png" />
+            </div>
+            <div className="post-card__body">
+              <p className="post-card__title">Post 1</p>
+              <p>
+                Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+                interpretaris, eam an quem pericula. Mei te aliquid
+                mediocritatem. Sint rebum repudiandae vel ei. In mollis fuisset
+                fierent pro, habemus dolores tincidunt ad nam. Vis laudem
+                nostrud ex, augue errem indoctum ea eam.
+              </p>
+              <div className="post-card__info">
+                <img
+                  className="post-card__avatar"
+                  alt="Avatar"
+                  src="../img/avatar.png"
+                />
+                <div>
+                  <p>Full name</p>
+                  <i className="fa fa-heart like-icon"></i>
+                  <span>20</span>
+                  <i className="fa fa-comment comment-icon"></i>
+                  <span>10</span>
+                </div>
+              </div>
+              <div className="post-card__overlay">
+                <button>View Details</button>
+                <button>Button2</button>
+              </div>
+            </div>
+          </div>
+          <div className="comments">
+            <h2>Comments</h2>
+            <div className="comment">
+              <div className="comment__user">
+                <img alt="Avatar" src="../img/avatar.png" />
+              </div>
+              <p className="comment__text">
+                Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+                interpretaris, eam an quem pericula. Mei te aliquid
+                mediocritatem. Sint rebum repudiandae vel ei. In mollis fuisset
+                fierent pro, habemus dolores tincidunt ad nam. Vis laudem
+                nostrud ex, augue errem indoctum ea eam.
+              </p>
+            </div>
+            <div className="comment">
+              <div className="comment__user">
+                <img alt="Avatar" src="../img/avatar.png" />
+              </div>
+              <p className="comment__text">
+                Lorem ipsum dolor sit amet, vel id nusquam repudiandae
+                interpretaris, eam an quem pericula. Mei te aliquid
+                mediocritatem. Sint rebum repudiandae vel ei. In mollis fuisset
+                fierent pro, habemus dolores tincidunt ad nam. Vis laudem
+                nostrud ex, augue errem indoctum ea eam.
+              </p>
+            </div>
+            <div>
+              <input placeholder="Add a comment" />
+            </div>
+          </div>
+        </div>
+      </div>
+
       {/* -------------- Inputs--------------- */}
       <div style={{ margin: "2rem auto", width: "50%" }}>
         <div
@@ -106,6 +348,7 @@ function App() {
       {/* ---------------Profile Card------------ */}
       <div
         style={{ backgroundColor: "#fdd343", width: "100%", padding: "1rem" }}
+        className="container"
       >
         {/* Supporter profile card */}
         <div className="profile-card profile-card--display-flex">
@@ -236,7 +479,7 @@ function App() {
       </div>
 
       {/* Filter*/}
-      <div style={{ margin: "3rem" }}>
+      <div className="container">
         <ul className="filter">
           <li className="filter-item">All Categories</li>
           <li className="filter-item filter-item--selected filter-item--before ">
