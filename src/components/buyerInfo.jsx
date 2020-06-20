d7016968d9af6de73e19ae4abd295e2c75939333
AmalAtef <AmalAtef@users.noreply.github.com>
2020-06-21 00:57:39 +0200

    Buyer Profile

--- src/components/buyerInfo.jsx
+++ src/components/buyerInfo.jsx
@@ -1,36 +1,36 @@
-import React from 'react';
+import React from "react";
 
-const BuyerInfo = () => {
-	return (
-		<>
-			<div className='profile-card'>
-				<div className='profile-card--display-flex'>
-					<span className='profile-card__title'>
-						<i className='fas fa-envelope profile-card__icon-sm '></i>Email
-					</span>
-					<p className='profile-card__text-primary--1'>buyer@gmail.com</p>
-				</div>
-				<div className='profile-card--display-flex'>
-					<span className='profile-card__title'>
-						<i className='fas fa-map-marker profile-card__icon-sm '></i>
-						Address
-					</span>
-					<p className='profile-card__text-primary--2'>6 October</p>
-				</div>
-				<div className='profile-card--display-flex'>
-					<span className='profile-card__title'>
-						<i className='fas fa-phone-alt  profile-card__icon-sm '></i>
-						phone
-					</span>
-					<p className='profile-card__text-primary--1'>01020304050</p>
-				</div>
+const BuyerInfo = ({ profile }) => {
+  return (
+    <>
+      <div className="profile-card">
+        <div className="profile-card--display-flex">
+          <span className="profile-card__title">
+            <i className="fas fa-envelope profile-card__icon-sm "></i>Email
+          </span>
+          <p className="profile-card__text-primary--1">{profile?.email}</p>
+        </div>
+        <div className="profile-card--display-flex">
+          <span className="profile-card__title">
+            <i className="fas fa-map-marker profile-card__icon-sm "></i>
+            Address
+          </span>
+          <p className="profile-card__text-primary--2">{profile?.address}</p>
+        </div>
+        <div className="profile-card--display-flex">
+          <span className="profile-card__title">
+            <i className="fas fa-phone-alt  profile-card__icon-sm "></i>
+            phone
+          </span>
+          <p className="profile-card__text-primary--1">{profile?.phone}</p>
+        </div>
 
-				<div className='profile-card__edit btn btn--circle btn--4'>
-					<i className='fas fa-pen'></i>
-				</div>
-			</div>
-		</>
-	);
+        <div className="profile-card__edit btn btn--circle btn--4">
+          <i className="fas fa-pen"></i>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default BuyerInfo;
