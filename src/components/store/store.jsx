8b9b03c5daf45e96f29f26576f95beee0f1edaad
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-20 21:37:37 +0200

    :bug: Fix Nav Links - Add Profile

--- src/components/store/store.jsx
+++ src/components/store/store.jsx
@@ -34,7 +34,7 @@ const Store = () => {
 				<div
 					style={{
 						display: 'grid',
-						gridTemplateColumns: 'auto auto auto',
+						gridTemplateColumns: 'repeat(3, minmax(0, 1fr))',
 						gap: '12rem',
 					}}
 				>
