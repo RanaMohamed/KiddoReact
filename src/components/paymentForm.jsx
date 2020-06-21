bac944936404002320de26087fcad326a153485b
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-21 19:21:41 +0200

    :sparkles: Buy Product

--- src/components/paymentForm.jsx
+++ src/components/paymentForm.jsx
@@ -1,47 +1,48 @@
-import React from 'react';
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
-import { useState } from 'react';
-import axios from '../axios';
-import { useSelector } from 'react-redux';
+import React from "react";
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import { useState } from "react";
+import axios from "../axios";
+import { useSelector } from "react-redux";
 
 const CARD_ELEMENT_OPTIONS = {
 	style: {
 		base: {
-			color: '#32325d',
+			color: "#32325d",
 			fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-			fontSmoothing: 'antialiased',
-			fontSize: '2rem',
-			'::placeholder': {
-				color: '#aab7c4',
+			fontSmoothing: "antialiased",
+			fontSize: "2rem",
+			"::placeholder": {
+				color: "#aab7c4",
 			},
 		},
 		invalid: {
-			color: '#fa755a',
-			iconColor: '#fa755a',
+			color: "#fa755a",
+			iconColor: "#fa755a",
 		},
 	},
 };
 
 const PaymentForm = ({ product, closeForm }) => {
 	const user = useSelector((state) => state.user.user);
-	const [error, setError] = useState('');
-	const [secret, setSecret] = useState(null);
+	const [loading, setLoading] = useState(false);
+	const [error, setError] = useState("");
 	const stripe = useStripe();
 	const elements = useElements();
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-		if (!secret) {
-			const { client_secret } = await axios.get(
-				`/secret?amount=${product?.price * 10}`
-			);
-			setSecret(client_secret);
-		}
-		if (!stripe || !elements || !secret) {
+		setLoading(true);
+
+		const { client_secret } = await axios.get(
+			`/secret?amount=${product?.price * 10}`
+		);
+
+		if (!stripe || !elements || !client_secret) {
+			setLoading(false);
 			return;
 		}
 
-		const result = await stripe.confirmCardPayment(secret, {
+		const result = await stripe.confirmCardPayment(client_secret, {
 			payment_method: {
 				card: elements.getElement(CardElement),
 				billing_details: {
@@ -53,30 +54,35 @@ const PaymentForm = ({ product, closeForm }) => {
 		if (result.error) {
 			// Show error to your customer (e.g., insufficient funds)
 			setError(result.error.message);
+			setLoading(false);
 		} else {
 			// The payment has been processed!
-			if (result.paymentIntent.status === 'succeeded') {
+			if (result.paymentIntent.status === "succeeded") {
 				console.log(result.paymentIntent);
 				await axios.post(`/product/buy/${product._id}`, {
 					payment: result.paymentIntent,
 				});
+				setLoading(false);
 				closeForm();
 			}
 		}
 	};
 
 	return (
-		<div className='modal'>
-			<div className='modal__body'>
+		<div className="modal">
+			<div className="modal__body">
 				<p>You are about to buy {product?.post?.title}</p>
-				<form onSubmit={handleSubmit}>
-					<label className='d-flex my-sm'>Card details</label>
+				<form className="form" onSubmit={handleSubmit}>
+					<label>Card details</label>
 					<CardElement options={CARD_ELEMENT_OPTIONS} />
-					<span className='error-message'>{error}</span>
-					<button className='btn btn--rect btn--1' disabled={!stripe}>
-						Confirm order
+					<span className="error-message">{error}</span>
+					<button
+						className={"btn btn--rect btn--1" + (loading ? " loading" : "")}
+						disabled={!stripe}
+					>
+						{!loading ? "Confirm order" : ""}
 					</button>
-					<button className='btn btn--rect btn--primary' disabled={!stripe}>
+					<button className="btn btn--rect btn--primary" onClick={closeForm}>
 						Cancel order
 					</button>
 				</form>
--- src/components/store/store.jsx
+++ src/components/store/store.jsx
@@ -5,60 +5,60 @@ import { getProducts, searchProduct } from "../../redux/actions/productAction";
 import Pagination from "../pagination";
 
 const Store = () => {
-  const products = useSelector((state) => state.product.products);
+	const products = useSelector((state) => state.product.products);
 
-  const currentPage = useSelector((state) => state.product.currentPage);
-  const perPage = useSelector((state) => state.product.perPage);
-  const [searchText, setsearchText] = useState({ searchText: "" });
+	const currentPage = useSelector((state) => state.product.currentPage);
+	const perPage = useSelector((state) => state.product.perPage);
+	const [searchText, setsearchText] = useState({ searchText: "" });
 
-  const dispatch = useDispatch();
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getProducts(currentPage, perPage));
-    dispatch(searchProduct(searchText.searchText));
-  }, [currentPage, searchText]);
+	useEffect(() => {
+		if (searchText.searchText) dispatch(searchProduct(searchText.searchText));
+		else dispatch(getProducts(currentPage, perPage));
+	}, [currentPage, searchText]);
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setsearchText({ searchText: e.target.value });
-  };
+	const handleChange = (e) => {
+		e.preventDefault();
+		setsearchText({ searchText: e.target.value });
+	};
 
-  return (
-    <>
-      <div
-        className="store"
-        style={{
-          margin: "2rem auto",
-        }}
-      >
-        <h2>Store</h2>
-        <input
-          type="search"
-          className="input input--text-color-primary input--border-primary input--padding-xs input--border-radius-md input--bg-info"
-          placeholder="search ..."
-          value={searchText.searchText}
-          onChange={handleChange}
-        />
-      </div>
-      <div className="container">
-        <div
-          style={{
-            display: "grid",
-            gridTemplateColumns: "auto auto auto",
-            gap: "12rem",
-          }}
-        >
-          {products.map((prod) => (
-            <Product key={prod._id} product={prod}></Product>
-          ))}
-        </div>
-      </div>
+	return (
+		<>
+			<div
+				className="store"
+				style={{
+					margin: "2rem auto",
+				}}
+			>
+				<h2>Store</h2>
+				<input
+					type="search"
+					className="input input--text-color-primary input--border-primary input--padding-xs input--border-radius-md input--bg-info"
+					placeholder="search ..."
+					value={searchText.searchText}
+					onChange={handleChange}
+				/>
+			</div>
+			<div className="container">
+				<div
+					style={{
+						display: "grid",
+						gridTemplateColumns: "auto auto auto",
+						gap: "12rem",
+					}}
+				>
+					{products.map((prod) => (
+						<Product key={prod._id} product={prod}></Product>
+					))}
+				</div>
+			</div>
 
-      <section className="pagination">
-        <Pagination reducer="product"></Pagination>
-      </section>
-    </>
-  );
+			<section className="pagination">
+				<Pagination reducer="product"></Pagination>
+			</section>
+		</>
+	);
 };
 
 export default Store;
