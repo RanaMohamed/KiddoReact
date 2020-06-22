91558f35e8f9515ec86c75d7e7ff74606b4df375
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-22 11:33:43 +0200

    :sparkles: Follow Category

--- src/pages/categories.jsx
+++ src/pages/categories.jsx
@@ -4,15 +4,24 @@ import Category from "../components/category";
 import Pagination from "./../components/pagination";
 import PostsList from "./../components/postslist";
 import { getSupportersCategory } from "./../redux/actions/userActions";
+import {
+	followCategory,
+	unfollowCategory,
+} from "../redux/actions/categoryActions";
 
 const Categories = () => {
 	const [showModal, setShowModal] = useState(false);
 	const supporters = useSelector((state) => state.user.supporters);
+	const user = useSelector((state) => state.user.user);
 	const [selectedCategory] = useSelector(
 		(state) => state.categories.selectedCategory
 	);
 
-	const joinCategoryHandler = () => {};
+	const joinCategoryHandler = () => {
+		user?.categories.indexOf(selectedCategory) === -1
+			? dispatch(followCategory(selectedCategory))
+			: dispatch(unfollowCategory(selectedCategory));
+	};
 
 	const dispatch = useDispatch();
 	useEffect(() => {
@@ -22,39 +31,45 @@ const Categories = () => {
 	return (
 		<>
 			<Category></Category>
-			<button
-				className="btn btn--rect btn--primary"
-				onClick={() => setShowModal(true)}
-			>
-				<i className="fas fa-headset"></i>
-				--Supporter
-			</button>
-			{showModal && (
-				<div className="modal">
-					<div className="modal__body">
-						<form>
-							<label className="d-flex my-sm">
-								Choose a Supporter to Help you
-							</label>
-							<div className="user-card">
-								{/* {supporters?.map((supporter) => (
+			{selectedCategory && (
+				<>
+					<button
+						className="btn btn--rect btn--primary"
+						onClick={() => setShowModal(true)}
+					>
+						<i className="fas fa-headset"></i>
+						--Supporter
+					</button>
+					{showModal && (
+						<div className="modal">
+							<div className="modal__body">
+								<form>
+									<label className="d-flex my-sm">
+										Choose a Supporter to Help you
+									</label>
+									<div className="user-card">
+										{/* {supporters?.map((supporter) => (
                   <p key={supporter._id}>
                     <div>---Supportter img---</div>
                     <div>{supporter.name}</div>
                   </p>
                 ))} */}
+									</div>
+								</form>
 							</div>
-						</form>
-					</div>
-				</div>
+						</div>
+					)}
+					<button
+						className="btn btn--rect btn--primary"
+						onClick={() => joinCategoryHandler()}
+					>
+						<i className="fas fa-users"></i>
+						{user?.categories?.indexOf(selectedCategory) !== -1
+							? "--Unfollow Category"
+							: "--Join Category"}
+					</button>
+				</>
 			)}
-			<button
-				className="btn btn--rect btn--primary"
-				onClick={() => joinCategoryHandler()}
-			>
-				<i className="fas fa-users"></i>
-				--Join Category
-			</button>
 			<PostsList></PostsList>
 			<Pagination reducer="post"></Pagination>
 		</>
--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -1,59 +1,81 @@
 import TYPES from "./types";
 
 const initialState = {
-  user: null,
-  profile: null,
-  token: localStorage.getItem("token"),
-  type: "",
-  supporters: [], //Basant to get category supporters
+	user: null,
+	profile: null,
+	token: localStorage.getItem("token"),
+	type: "",
+	supporters: [], //Basant to get category supporters
 };
 const userReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case TYPES.LOGIN_USER:
-    case TYPES.SIGNUP_KID: {
-      localStorage.setItem("token", action.payload.token);
-      return {
-        ...state,
-        user: action.payload.user,
-        token: action.payload.token,
-        type: action.payload.type,
-        errors: {},
-      };
-    }
-    case TYPES.LOAD_USER:
-      return {
-        ...state,
-        user: action.payload.user,
-        type: action.payload.type,
-        errors: {},
-      };
-    case TYPES.LOGOUT:
-      return {
-        ...state,
-        user: null,
-        type: "",
-        token: null,
-        errors: {},
-      };
+	switch (action.type) {
+		case TYPES.LOGIN_USER:
+		case TYPES.SIGNUP_KID: {
+			localStorage.setItem("token", action.payload.token);
+			return {
+				...state,
+				user: action.payload.user,
+				token: action.payload.token,
+				type: action.payload.type,
+				errors: {},
+			};
+		}
+		case TYPES.LOAD_USER:
+			return {
+				...state,
+				user: action.payload.user,
+				type: action.payload.type,
+				errors: {},
+			};
+		case TYPES.LOGOUT:
+			return {
+				...state,
+				user: null,
+				type: "",
+				token: null,
+				errors: {},
+			};
 
-    case TYPES.GET_PROFILE:
-      return {
-        ...state,
-        profile: action.payload.profile,
-      };
-    case TYPES.CHANGE_NAME:
-      return {
-        ...state,
-        user: { ...state.user, name: action.payload },
-      };
-    case TYPES.GET_SUPPORTERS_CATEGORY:
-      return {
-        ...state,
-        user: { ...state.user, id: action.payload },
-      };
-    default:
-      return state;
-  }
+		case TYPES.GET_PROFILE:
+			return {
+				...state,
+				profile: action.payload.profile,
+			};
+		case TYPES.CHANGE_NAME:
+			return {
+				...state,
+				user: { ...state.user, name: action.payload },
+			};
+		case TYPES.GET_SUPPORTERS_CATEGORY:
+			return {
+				...state,
+				user: { ...state.user, id: action.payload },
+			};
+		case TYPES.FOLLOW_CATEGORY: {
+			const user = {
+				...state.user,
+				categories: [state.user.categories, action.payload.category],
+			};
+			return {
+				...state,
+				user: user,
+			};
+		}
+		case TYPES.UNFOLLOW_CATEGORY: {
+			const user = {
+				...state.user,
+				categories: state.user.categories.filter(
+					(cat) => cat !== action.payload.category
+				),
+			};
+			return {
+				...state,
+				user: user,
+			};
+		}
+		default:
+			return state;
+	}
 };
 
 export default userReducer;
