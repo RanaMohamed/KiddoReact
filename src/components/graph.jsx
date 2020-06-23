6cd40afca60d3859415db29a19a34a58978fe478
Basant-innovation <59922781+Basant-innovation@users.noreply.github.com>
2020-06-23 11:53:45 +0200

    :lipstick: Updating Styles

---
+++ src/components/graph.jsx
@@ -0,0 +1,351 @@
+import React from "react";
+const Graph = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xlink="http://www.w3.org/1999/xlink"
+        width="100%"
+        height="100%"
+        viewBox="0 0 1611.51 1778.304"
+      >
+        <defs>
+          <filter
+            id="Ellipse_10"
+            x="102.51"
+            y="120.304"
+            width="223"
+            height="223"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dy="3" input="SourceAlpha" />
+            <feGaussianBlur stdDeviation="3" result="blur" />
+            <feFlood flood-opacity="0.161" />
+            <feComposite operator="in" in2="blur" />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="Ellipse_11"
+            x="867.51"
+            y="315.304"
+            width="223"
+            height="223"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dy="3" input="SourceAlpha" />
+            <feGaussianBlur stdDeviation="3" result="blur-2" />
+            <feFlood flood-opacity="0.161" />
+            <feComposite operator="in" in2="blur-2" />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="Ellipse_12"
+            x="1078.51"
+            y="1068.304"
+            width="223"
+            height="223"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dy="3" input="SourceAlpha" />
+            <feGaussianBlur stdDeviation="3" result="blur-3" />
+            <feFlood flood-opacity="0.161" />
+            <feComposite operator="in" in2="blur-3" />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="Ellipse_13"
+            x="152.932"
+            y="744.304"
+            width="223"
+            height="223"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dy="3" input="SourceAlpha" />
+            <feGaussianBlur stdDeviation="3" result="blur-4" />
+            <feFlood flood-opacity="0.161" />
+            <feComposite operator="in" in2="blur-4" />
+            <feComposite in="SourceGraphic" />
+          </filter>
+          <filter
+            id="Ellipse_14"
+            x="113.932"
+            y="1441.65"
+            width="223"
+            height="223"
+            filterUnits="userSpaceOnUse"
+          >
+            <feOffset dy="3" input="SourceAlpha" />
+            <feGaussianBlur stdDeviation="3" result="blur-5" />
+            <feFlood flood-opacity="0.161" />
+            <feComposite operator="in" in2="blur-5" />
+            <feComposite in="SourceGraphic" />
+          </filter>
+        </defs>
+        <g id="Graph" transform="translate(-169.49 -2395.696)">
+          <path
+            id="Path_107"
+            data-name="Path 107"
+            d="M1573,8983.175s351.586-158.606,481.453-26.61,102.19,166.06,219.284,210.768,481.147,80.9,400.246,298.056-843.071,108.577-981.454,76.643-274.637,75.5-285.282,156.97,14.9,326.307,208.639,311.4,600.369-166.06,715.333-132,432.18,87.287,263.992,353.408-921.843,163.93-1038.936,144.77"
+            transform="translate(-1114 -6359)"
+            fill="none"
+            stroke="#e55658"
+            stroke-width="4"
+            stroke-dasharray="14"
+          />
+          <g id="know" transform="translate(3)">
+            <path
+              id="Path_102"
+              data-name="Path 102"
+              d="M1232.444,7816.409c-1.521-95.844,187.124-252.541,261.669-251.02s34.845,79.779,61.953,142.534,109.957-10.178,108.435,96.315-128.933,189.786-217.55,214.507-108.015-85.194-136.92-115.621S1233.965,7912.252,1232.444,7816.409Z"
+              transform="translate(-1065.944 -5169.682)"
+              fill="#fdd343"
+            />
+            <g
+              transform="matrix(1, 0, 0, 1, 166.49, 2395.7)"
+              filter="url(#Ellipse_10)"
+            >
+              <circle
+                id="Ellipse_10-2"
+                data-name="Ellipse 10"
+                cx="102.5"
+                cy="102.5"
+                r="102.5"
+                transform="translate(111.51 126.3)"
+                fill="#fff"
+              />
+            </g>
+            <text
+              id="Know_"
+              data-name="Know "
+              transform="translate(307 2644)"
+              fill="#106d74"
+              font-size="48"
+              font-family="PermanentMarker-Regular, Permanent Marker"
+            >
+              <tspan x="0" y="0">
+                Know{" "}
+              </tspan>
+            </text>
+          </g>
+          <g id="interact" transform="translate(3)">
+            <path
+              id="Path_103"
+              data-name="Path 103"
+              d="M1232.444,7816.409c-1.521-95.844,187.124-252.541,261.669-251.02s34.845,79.779,61.953,142.534,109.957-10.178,108.435,96.315-128.933,189.786-217.55,214.507-108.015-85.194-136.92-115.621S1233.965,7912.252,1232.444,7816.409Z"
+              transform="translate(-300.944 -4974.682)"
+              fill="#fdd343"
+            />
+            <g
+              transform="matrix(1, 0, 0, 1, 166.49, 2395.7)"
+              filter="url(#Ellipse_11)"
+            >
+              <circle
+                id="Ellipse_11-2"
+                data-name="Ellipse 11"
+                cx="102.5"
+                cy="102.5"
+                r="102.5"
+                transform="translate(876.51 321.3)"
+                fill="#fff"
+              />
+            </g>
+            <text
+              id="Interact-2"
+              data-name="Interact"
+              transform="translate(1066 2835)"
+              fill="#106d74"
+              font-size="39"
+              font-family="PermanentMarker-Regular, Permanent Marker"
+            >
+              <tspan x="0" y="0">
+                Interact
+              </tspan>
+            </text>
+          </g>
+          <g id="do" transform="translate(157 96)">
+            <path
+              id="Path_104"
+              data-name="Path 104"
+              d="M1232.444,7816.409c-1.521-95.844,187.124-252.541,261.669-251.02s34.845,79.779,61.953,142.534,109.957-10.178,108.435,96.315-128.933,189.786-217.55,214.507-108.015-85.194-136.92-115.621S1233.965,7912.252,1232.444,7816.409Z"
+              transform="translate(-243.944 -4317.682)"
+              fill="#fdd343"
+            />
+            <g
+              transform="matrix(1, 0, 0, 1, 12.49, 2299.7)"
+              filter="url(#Ellipse_12)"
+            >
+              <circle
+                id="Ellipse_12-2"
+                data-name="Ellipse 12"
+                cx="102.5"
+                cy="102.5"
+                r="102.5"
+                transform="translate(1087.51 1074.3)"
+                fill="#fff"
+              />
+            </g>
+            <text
+              id="do-2"
+              data-name="do"
+              transform="translate(1178 3495)"
+              fill="#106d74"
+              font-size="48"
+              font-family="PermanentMarker-Regular, Permanent Marker"
+            >
+              <tspan x="0" y="0">
+                do
+              </tspan>
+            </text>
+          </g>
+          <g id="develop" transform="translate(3)">
+            <path
+              id="Path_105"
+              data-name="Path 105"
+              d="M1232.444,7816.409c-1.521-95.844,187.124-252.541,261.669-251.02s34.845,79.779,61.953,142.534,109.957-10.178,108.435,96.315-128.933,189.786-217.55,214.507-108.015-85.194-136.92-115.621S1233.965,7912.252,1232.444,7816.409Z"
+              transform="translate(-1015.522 -4545.682)"
+              fill="#fdd343"
+            />
+            <g
+              transform="matrix(1, 0, 0, 1, 166.49, 2395.7)"
+              filter="url(#Ellipse_13)"
+            >
+              <circle
+                id="Ellipse_13-2"
+                data-name="Ellipse 13"
+                cx="102.5"
+                cy="102.5"
+                r="102.5"
+                transform="translate(161.93 750.3)"
+                fill="#fff"
+              />
+            </g>
+            <text
+              id="DEvelop_"
+              data-name="DEvelop "
+              transform="translate(345 3265)"
+              fill="#106d74"
+              font-size="41"
+              font-family="PermanentMarker-Regular, Permanent Marker"
+            >
+              <tspan x="0" y="0">
+                DEvelop{" "}
+              </tspan>
+            </text>
+          </g>
+          <g id="offer" transform="translate(3)">
+            <path
+              id="Path_106"
+              data-name="Path 106"
+              d="M1232.444,7816.409c-1.521-95.844,187.124-252.541,261.669-251.02s34.845,79.779,61.953,142.534,109.957-10.178,108.435,96.315-128.933,189.786-217.55,214.507-108.015-85.194-136.92-115.621S1233.965,7912.252,1232.444,7816.409Z"
+              transform="translate(-1054.522 -3848.336)"
+              fill="#fdd343"
+            />
+            <g
+              transform="matrix(1, 0, 0, 1, 166.49, 2395.7)"
+              filter="url(#Ellipse_14)"
+            >
+              <circle
+                id="Ellipse_14-2"
+                data-name="Ellipse 14"
+                cx="102.5"
+                cy="102.5"
+                r="102.5"
+                transform="translate(122.93 1447.65)"
+                fill="#fff"
+              />
+            </g>
+            <text
+              id="offer-2"
+              data-name="offer"
+              transform="translate(329.423 3965.346)"
+              fill="#106d74"
+              font-size="48"
+              font-family="PermanentMarker-Regular, Permanent Marker"
+            >
+              <tspan x="0" y="0">
+                offer
+              </tspan>
+            </text>
+          </g>
+          <g id="Text">
+            <text
+              id="Know_your_interests_and_skills"
+              data-name="Know your interests and skills"
+              transform="translate(513 2504)"
+              fill="#33312c"
+              font-size="48"
+              font-family="Tajawal-Medium, Tajawal"
+              font-weight="500"
+            >
+              <tspan x="0" y="0">
+                Know your interests and skills
+              </tspan>
+            </text>
+            <text
+              id="Offer_your_products_for_sale"
+              data-name="Offer your products for sale"
+              transform="translate(533 3840)"
+              fill="#33312c"
+              font-size="48"
+              font-family="Tajawal-Medium, Tajawal"
+              font-weight="500"
+            >
+              <tspan x="0" y="0">
+                Offer your products for sale
+              </tspan>
+            </text>
+            <text
+              id="Develop_yourself_in_different_skills"
+              data-name="Develop yourself in different skills"
+              transform="translate(552 3028)"
+              fill="#33312c"
+              font-size="48"
+              font-family="Tajawal-Medium, Tajawal"
+              font-weight="500"
+            >
+              <tspan x="0" y="49">
+                Develop yourself in{" "}
+              </tspan>
+              <tspan x="0" y="116">
+                different skills
+              </tspan>
+            </text>
+            <text
+              id="Interact_with_other_kids_with_the_same_interests"
+              data-name="Interact with other kids with the same interests"
+              transform="translate(1271 2622)"
+              fill="#33312c"
+              font-size="48"
+              font-family="Tajawal-Medium, Tajawal"
+              font-weight="500"
+            >
+              <tspan x="0" y="49">
+                Interact with other kids{" "}
+              </tspan>
+              <tspan x="0" y="116">
+                with the same interests
+              </tspan>
+            </text>
+            <text
+              id="Do_what_you_love_to_do"
+              data-name="Do what you love to do"
+              transform="translate(1481 3354)"
+              fill="#33312c"
+              font-size="48"
+              font-family="Tajawal-Medium, Tajawal"
+              font-weight="500"
+            >
+              <tspan x="0" y="49">
+                Do what you{" "}
+              </tspan>
+              <tspan x="0" y="116">
+                love to do
+              </tspan>
+            </text>
+          </g>
+        </g>
+      </svg>
+    </>
+  );
+};
+export default Graph;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ContactForm from "../components/contactForm";
 
 const Contact = () => {
-	return (
-		<>
-			<ContactForm></ContactForm>
-		</>
-	);
+  return (
+    <>
+      <ContactForm></ContactForm>
+    </>
+  );
 };
 
 export default Contact;
