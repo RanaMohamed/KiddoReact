5d878e1742121e89d648d0c23de952a04e2225c5
RanaMohamed <rana.mohamed.97@hotmail.com>
2020-06-23 02:11:20 +0200

    :bug: Fixinggg

--- src/components/contactForm.jsx
+++ src/components/contactForm.jsx
@@ -1,41 +1,46 @@
 import React from "react";
 
 const ContactForm = () => {
-  return (
-    <>
-      <section className="my-lg">
-        <div className="container my-xl">
-          <h1>contact Us</h1>
-          <form className="form">
-            <label htmlFor="title" className="form__subtitle">
-              Name
-            </label>
-            <input
-              type="text"
-              className="input form__input-1 input--text-color-info input--padding-sm input--border-radius-md input--border-info"
-              placeholder="Write your Name"
-            />
-            <label htmlFor="title" className="form__subtitle">
-              Email
-            </label>
-            <input
-              type="text"
-              className="input form__input-1 input--text-color-info input--padding-sm input--border-radius-md input--border-info"
-              placeholder="Write Your Email"
-            />
-            <label htmlFor="title" className="form__subtitle">
-              Your Message
-            </label>
-            <textarea
-              type="text"
-              className="input input--text-color-primary input--bg-secondary input--padding-xs input--border-radius-md"
-              placeholder="write your message here..."
-            />
-          </form>
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<section className="my-lg">
+				<div className="container my-xl">
+					<h1>contact Us</h1>
+					<form className="form">
+						<label htmlFor="title" className="form__subtitle">
+							Name
+						</label>
+						<input
+							type="text"
+							className="input input--text-color-info input--padding-sm input--border-radius-md input--border-info"
+							placeholder="Write your Name"
+						/>
+						<span className="error-message"></span>
+						<label htmlFor="title" className="form__subtitle">
+							Email
+						</label>
+						<input
+							type="text"
+							className="input input--text-color-info input--padding-sm input--border-radius-md input--border-info"
+							placeholder="Write Your Email"
+						/>
+						<span className="error-message"></span>
+						<label htmlFor="title" className="form__subtitle">
+							Your Message
+						</label>
+						<textarea
+							type="text"
+							className="input input--text-color-primary input--bg-secondary input--padding-xs input--border-radius-md input--border-info"
+							placeholder="write your message here..."
+						/>
+						<span className="error-message"></span>
+
+						<button className="btn btn--primary btn--rect">Send</button>
+					</form>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default ContactForm;
